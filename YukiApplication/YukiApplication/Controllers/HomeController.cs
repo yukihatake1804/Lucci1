85c5b58b525744359857e2bf05acdf9d88a61b47
RicardoMilos <nhokhieuthao@gmail.com>
2020-07-11 14:21:08 +0700

    Add Process
    Add new

---
+++ YukiApplication/YukiApplication/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YukiApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Woman()
+        {
+            ViewBag.Message = "Your woman page.";
+
+            return View();
+        }
+
+        public ActionResult Men()
+        {
+            ViewBag.Message = "Your men page.";
+
+            return View();
+        }
+    }
+}
---
+++ YukiApplication/YukiApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YukiApplication.MvcApplication" Language="C#" %>
